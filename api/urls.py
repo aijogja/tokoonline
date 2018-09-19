82a301438ae4555d12b42321fb2e97daa4bd082e
Ai Jogja <cah7x_jogja@yahoo.com>
2018-09-19 11:42:36 +0700

    auth token

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework.authtoken import views
 
 schema_view = get_swagger_view(title='TokoOnline API')
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('docs', schema_view),
     path('kategori/', include('api.kategori.urls')),
     path('produk/', include('api.produk.urls')),
+    path('auth-token/', views.obtain_auth_token)
 ]
 
