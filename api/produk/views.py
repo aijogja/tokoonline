a24eecc2fc35c86020dc9ef45057d58d3d0d9293
Ai Jogja <cah7x.jogja@gmail.com>
2018-09-28 15:13:51 +0700

    retrieve api

--- api/produk/views.py
+++ api/produk/views.py
@@ -6,3 +6,8 @@ from api.produk.serializers import ProdukSerializer
 class ProdukList(generics.ListAPIView):
     queryset = Produk.objects.all()
     serializer_class = ProdukSerializer
+
+
+class ProdukDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Produk.objects.all()
+    serializer_class = ProdukSerializer
