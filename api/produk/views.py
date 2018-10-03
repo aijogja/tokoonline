6d6096cd9a249da86a77bed4cf422c932958e73d
Ai Jogja <cah7x_jogja@yahoo.com>
2018-10-03 11:36:12 +0700

    filtering data

--- api/produk/views.py
+++ api/produk/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from product.models import Produk
 from api.produk.serializers import ProdukSerializer
 from api.permissions import IsAdminOrReadOnly
@@ -8,6 +9,8 @@ class ProdukList(generics.ListCreateAPIView):
     queryset = Produk.objects.all()
     serializer_class = ProdukSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('kategori',)
 
 
 class ProdukDetail(generics.RetrieveUpdateDestroyAPIView):
