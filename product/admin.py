b9b0c381a0e675c6b5b18f1540fd249cef27c730
Ai Jogja <cah7x_jogja@yahoo.com>
2018-10-08 12:55:48 +0700

    order models

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from product.models import Kategori, Produk
+from product.models import Kategori, Produk, Order
 
 # Register your models here.
 
 admin.site.register(Kategori)
 admin.site.register(Produk)
+admin.site.register(Order)
--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -19,3 +20,28 @@ class Kategori(models.Model):
 
     def __str__(self):
         return self.nama
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True)
+    alamat = models.TextField()
+    ongkir = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    qty = models.IntegerField(default=1)
+    produk = models.ForeignKey('Produk', on_delete=models.CASCADE)
+    catatan = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        choices=(
+            ('checkout', 'Checkout'),
+            ('paid', 'Terbayar'),
+            ('delivered', 'Terkirim'),
+        ),
+        default='checkout',
+        max_length=10,
+    )
+
+    def __str__(self):
+        return str(self.pk)
