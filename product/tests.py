d4c94529ae0fd51dd43d26e265d128767cd7922e
Ai Jogja <cah7x_jogja@yahoo.com>
2018-10-08 09:42:26 +0700

    unittest

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 # Create your tests here.
+
+class TokenTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="aijogja", password="zxcvbnm,")
+
+    def test_token_on_user_created(self):
+        user = User.objects.get(username="aijogja")
+        self.assertIsNotNone(user.auth_token)
