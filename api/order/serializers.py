8545d27fc7873879268fb44f9fe57d3df7957983
Ai Jogja <cah7x_jogja@yahoo.com>
2018-10-26 16:04:22 +0700

    serializer bersarang, memisahkan serializer untuk GET dan POST

--- api/order/serializers.py
+++ api/order/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from product.models import Order, OrderBarang
+from api.produk.serializers import ProdukSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -8,18 +9,34 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ('id', 'no_hape', 'alamat', 'ongkir', 'catatan')
 
     def create(self, validated_data):
+        # Digunakan untuk biar ketika order, otomatis mengisi user berdasarkan
+        # siapa yang sedang aktif
         user = self.context['request'].user
         validated_data['user'] = user
         return super(OrderSerializer, self).create(validated_data)
 
 
-class OrderBarangSerializer(serializers.ModelSerializer):
+class OrderBarangGETSerializer(serializers.ModelSerializer):
+    # ProdukSerializer() ini digunakan untuk menampilkan detail data produk
+    produk = ProdukSerializer()
+
+    class Meta:
+        model = OrderBarang
+        fields = ('id', 'produk', 'harga', 'qty')
+
+
+class OrderBarangPOSTSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = OrderBarang
         fields = ('id', 'produk', 'harga', 'qty')
 
     def create(self, validated_data):
+        # Digunakan untuk biar ketika order barang, otomatis menggunakan
+        # order id yang sedang dipakai
         order_id = self.context['order_id']
         order = Order.objects.get(id=order_id)
+        produk = validated_data['produk']
         validated_data['order'] = order
-        return super(OrderBarangSerializer, self).create(validated_data)
+        validated_data['harga'] = produk.harga
+        return super(OrderBarangPOSTSerializer, self).create(validated_data)
--- api/order/views.py
+++ api/order/views.py
@@ -1,14 +1,22 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from product.models import Order, OrderBarang
-from api.order.serializers import OrderSerializer, OrderBarangSerializer
+from api.order.serializers import (
+    OrderSerializer,
+    OrderBarangGETSerializer,
+    OrderBarangPOSTSerializer,
+)
 
 
 class OrderList(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('status',)
 
     def get_queryset(self):
+        # Untuk menampilkan data Order berdasarkan user yang login
         user = self.request.user
         orders = Order.objects.filter(user=user)
         return orders
@@ -22,15 +30,22 @@ class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class OrderBarangList(generics.ListCreateAPIView):
     queryset = OrderBarang.objects.all()
-    serializer_class = OrderBarangSerializer
+    serializer_class = OrderBarangGETSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return OrderBarangGETSerializer
+        return OrderBarangPOSTSerializer
+
     def get_queryset(self):
+        # Untuk menampilkan data OrderBarang berdasarkan id Order
         pk = self.kwargs.get('pk')
         orders = OrderBarang.objects.filter(order=pk)
         return orders
 
     def get_serializer_context(self):
+        # Untuk mengirimkan context ke serializer
         pk = self.kwargs.get('pk')
         context = super(OrderBarangList, self).get_serializer_context()
         context['order_id'] = pk
